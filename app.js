9620e4029c67b0bd2aee80b32255a91878e6e880
vungnv <vungnv@avtvn.com>
2024-12-22 16:05:34 +0700

    update file name

--- app.js
+++ app.js
@@ -2,21 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRouter from './routers/userRouter.js';
-import todoRouter from './routers/todoRouter.js';
+import todoRouter from './routers/todoRouter1.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+// Kết nối tới MongoDB
+connectDB(); // Gọi hàm connectDB để kết nối cơ sở dữ liệu
 
 // Middleware
-app.use(express.json()); 
-app.use('/api/users', userRouter); 
-app.use('/api/todos', todoRouter); 
+app.use(express.json());
+app.use('/api/users', userRouter);
+app.use('/api/todos', todoRouter);
 
 // Khởi động server
 app.listen(PORT, () => {
